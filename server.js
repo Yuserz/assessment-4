6e1f7da2e5e3f03ce3ea5f62181695c40ba53681
Yusri Caloyloy <61104011+Yuserz@users.noreply.github.com>
2023-02-07 22:11:07 +0800

    feat: add CRUDE methods for users data
    changes:
    1. delete app.js from root
    2. replaced with server.js
    
    features:
    1. add mongodb

---
+++ server.js
@@ -0,0 +1,120 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongodb = require("mongodb");
+const { mongo } = require("mongoose");
+const MongoClient = mongodb.MongoClient;
+
+const app = express();
+const port = 3000;
+const url = "mongodb://localhost:27017";
+
+app.use(bodyParser.json());
+
+let db;
+
+MongoClient.connect(url, { useNewUrlParser: true }, (error, client) => {
+  if (error) {
+    console.log("Error connecting to MongoDB: ", error);
+    return;
+  }
+
+  db = client.db("MyData");
+  console.log("Connected to MongoDB successfully.");
+
+  //Schema
+  const emailAndPasswordSchema = {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  };
+
+  //POST method
+  //Define a new collection to store the email and password
+  const collection = db.collection("user");
+
+  //Create Data
+  app.post("/api/users", (req, res) => {
+    const { email, password } = req.body;
+
+    // Validate the request body against the schema
+    if (!email || !password) {
+      return res
+        .status(400)
+        .send({ error: "Both email and password are required" });
+    }
+
+    collection
+      .insertOne({ email, password })
+      .then((result) => {
+        res.send({ success: "Email and password stored successfully" });
+      })
+      .catch((error) => {
+        console.log(
+          "Error inserting email and password into the database: ",
+          error
+        );
+        res.status(500).send({
+          error: "Error inserting email and password into the database",
+        });
+      });
+  });
+
+  //Get all users data
+  app.get("/api/users", (req, res) => {
+    db.collection("user")
+      .find({})
+      .toArray((error, result) => {
+        if (error) {
+          console.log("Error retrieving data from the database: ", error);
+          return;
+        }
+        res.send(result);
+      });
+  });
+
+  //Delete user with id
+  app.delete("/api/users/:id", (req, res) => {
+    const id = req.params.id;
+    collection
+      .deleteOne({ _id: new mongodb.ObjectId(id) })
+      .then((result) => {
+        res.send({ success: "Data deleted successfully" });
+      })
+      .catch((error) => {
+        console.log("Error deleting data from the database: ", error);
+        res
+          .status(500)
+          .send({ error: "Error deleting data from the database" });
+      });
+  });
+
+  //Update user data
+  app.put("/api/users/:id", (req, res) => {
+    const id = req.params.id;
+    const userUpdates = req.body;
+
+    db.collection("user").findOneAndUpdate(
+      { _id: mongodb.ObjectId(id) },
+      { $set: userUpdates },
+      { returnOriginal: false },
+      (error, result) => {
+        if (error) {
+          console.log("Error updating user: ", error);
+          res.status(500).send({ error: "Error updating user" });
+          return;
+        }
+
+        res.send(result.value);
+      }
+    );
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+});
