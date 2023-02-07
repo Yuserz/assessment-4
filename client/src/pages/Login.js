602be36366f9703cef03d5979c6b6644c6687cb7
Yusri Caloyloy <61104011+Yuserz@users.noreply.github.com>
2023-02-07 09:43:05 +0800

    feat: add Tailwind Framework

---
+++ client/src/pages/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+  
+export default function Login() {
+
+const initialState = {
+  email: "",
+  password: "",
+}
+
+  return (
+    <div className="flex items-center justify-evenly  m-auto pl-5 pr-5 max-w-[1240px] h-screen">
+     
+    </div>   
+  )
+}
